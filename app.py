ae653b9ecc4c8af7228f1252d5c6c605258045e6
mfleming711 <mfleming.duck@gmail.com>
2024-09-26 03:37:03 -0400

    add more apis

--- app.py
+++ app.py
@@ -5,10 +5,27 @@ app = Flask(__name__)
 
 @app.route('/run')
 def run():
-    with open('echo.json', 'r') as file:
+    with open('occupation.json', 'r') as file:
         data = json.load(file)
     return jsonify(data)
 
+@app.route('/lidar-hub/api/v1/event_zones')
+def event_zones():
+    with open('event_zones.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/lidar-hub/api/v1/event_zones/realtime')
+def event_zones_realtime():
+    with open('event_zones_realtime.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
+
+@app.route('/lidar-hub/api/v1/event_zones/timeseries')
+def event_zones_timeseries():
+    with open('event_zones_timeseries.json', 'r') as file:
+        data = json.load(file)
+    return jsonify(data)
 
 if __name__ == '__main__':
     app.run(debug=True)
